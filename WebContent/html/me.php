fe66ae2ff57595ef657f859dd1b77324c4fb9f84
Da-Jin Chu <dajinchu@gmail.com>
2015-09-08 13:01:25 -0400

    Added content to me, and cards w/ content to portfolio. No need for subtab

--- WebContent/html/me.php
+++ WebContent/html/me.php
@@ -18,15 +18,36 @@
 	</div>
 	
 	<div id = "body-background">
-	<div id = "body-card-buffer">
+	<div class = "body-card-buffer stretch-to-bottom">
 	<div class = "carddiv">
-		<p>^that's me</p>
+		<h3>HELLO</h3>
+		<h4>my name is Da-Jin.</h4>
+		<p>My real name is 朱大靖, which is pronounced zhu da jing, hence my official english name. 
+		My rapper name is dj chu, and my indian name is diraj. I was born in Winchester, but my mom is from Malaysia, and my dad is from Taiwan. 
+		They're both native chinese speakers, and got their American citizenships a few years ago.</p>
+		<p>Currently, I am a junior at Mass Academy of Math and Science, and I drive forty minutes every morning to get from Bedford to Worcester.
+		Because we moved here from Waltham when I was three, I can't remember anything outside of Bedford, so 
+		I've always known the same 180 kids, and the switch to Mass Academy was pretty significant.
+		Luckily, it appears to be the right decision so far. I'm not falling asleep in class anymore, and we do cool stuff like build websites.</p>
 		<!--<p>
 		My name ain't Da-Jin<br>
 		You're just imagining<br>
 		It's dj chu<br>
 		And I'd love to meet you<br>
 		</p>-->
+	</div>
+	<div class = "carddiv">
+		<h3>Log Frog</h3>
+		<h4>A game I published on Google Play</h4>
+		<pre>Log frog is a simple abstract puzzle game.
+Connect, rotate, and leap-frog logs to get to the green circle.
+Moving a log next to another log allows you to move any connected log.
+Includes 40 unique levels that increase in challenge; many will leave you stumped!</pre>
+<a href="https://play.google.com/store/apps/details?id=com.gmail.dajinchu.logfrog">
+  <img alt="Get it on Google Play"
+       src="https://googledrive.com/host/0B7V56SUAoohNfmpJaW1VY0FLVUpISmxXSEs3Z0ZQWWc4MkZZVUFNaG9YTmQybUFCc3J4M3M/logfrogicon.png"
+       width = "100px" height = "100px" />
+</a>
 	</div>
 	</div>
 	</div>
--- WebContent/html/portfolio.php
+++ WebContent/html/portfolio.php
@@ -19,13 +19,29 @@
 		</div>
 	
 	<div id = "body-background">
-	<div id = "body-card-buffer">
-	<div class = "carddiv">
-		<p>Use the portfolio tab to see what we do in each subject at Mass Academy.</p>
+		<div class = "body-card-buffer">
+			<div class = "carddiv">
+				<h3>Computer Science</h3>
+				<h4>Ms. Torrico</h4>
+				<p>This is a website for CS at Mass Academy.</p>
+			</div>
+			<div class = "carddiv">
+				<h3>STEM</h3>
+				<h4>Ms. Borowski</h4>
+				<p>STEM is a class focused on the two biggest projects of the year, STEM I and STEM II. </p>
+			</div>
+			<div class = "carddiv">
+				<h3>Scientific and Technical Writing</h3>
+				<h4>Ms. Borowski</h4>
+				<p>STW, like STEM, is also taught by Ms. Borowski, and so far, no one can tell the difference between the two classes, though Mr. Connell does show up during STEM.</p>
+			</div>
+			<div class = "carddiv">
+				<h3>Humanities</h3>
+				<h4>Mr. Ludt</h4>
+				<p>Humanities is History and English class combined into a big discussion thing. Every week we write a journal discussing the past week, a grammar error in the wild, and 20 new vocab words we've learned.</p>
+			</div>
+		</div>
 	</div>
-	</div>
-	</div>
-	
 	</div>
 </body>
 </html>
