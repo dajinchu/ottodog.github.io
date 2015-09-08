1e1b116453998842948f6c38cae1559634c0e856
Da-Jin Chu <dajinchu@gmail.com>
2015-09-08 13:07:25 -0400

    Actually deleted sub-tabs

--- WebContent/html/menu.php
+++ WebContent/html/menu.php
@@ -3,16 +3,6 @@
   		<div class = "tab welcome"> <a href="/html/index.php">WELCOME</a></div>
   		<div class = "tab me"> <a href="/html/me.php">ME</a></div>
   		<div class = "tab portfolio"> <a href="/html/portfolio.php">PORTFOLIO</a>
-  			<div id="subject-container">
-  				
-<div class = "cs"> <a href="/html/subjects/cs.php">CS</a></div>
-<div class = "stem"> <a href="stem.php">STEM</a></div>
-<div class = "stw"> <a href="stem.php">STW</a></div>
-<div class = "hum"> <a href="hum.php">HUM</a></div>
-<div class = "math"> <a href="stem.php">MATH</a></div>
-<div class = "french"> <a href="stem.php">FRENCH</a></div>
-<div class = "physics"> <a href="stem.php">PHYSICS</a></div>
-  			</div>
   		</div>  		
 	</div>
 </div>
