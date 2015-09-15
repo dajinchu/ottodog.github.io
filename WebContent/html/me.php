a8d403adb0f461c5ef8500eb610afd59520bd1b0
Da-Jin Chu <dajinchu@gmail.com>
2015-09-14 21:39:04 -0400

    Wrote more content

--- WebContent/html/me.php
+++ WebContent/html/me.php
@@ -23,35 +23,46 @@
 		<h3>HELLO</h3>
 		<h4>my name is Da-Jin.</h4>
 		<p>My real name is 朱大靖, which is pronounced zhu da jing, hence my official english name. 
-		My rapper name is dj chu, and my indian name is diraj. I was born in Winchester, but my mom is from Malaysia, and my dad is from Taiwan. 
-		They're both native chinese speakers, and got their American citizenships a few years ago.</p>
-		<p>Currently, I am a junior at Mass Academy of Math and Science, and I drive forty minutes every morning to get from Bedford to Worcester.
+		<!--My rapper name is dj chu, and my indian name is diraj.--> I was born in Winchester, but my mom is from Malaysia, and my dad is from Taiwan. 
+		They're both native chinese speakers, but have been permanent residents for over a decade, and got their American citizenships a few years ago.</p>
+		<p>Currently, I am a junior at Mass Academy of Math and Science, and it's a forty minute drive every morning to get from Bedford to Worcester.
 		Because we moved here from Waltham when I was three, I can't remember anything outside of Bedford, so 
 		I've always known the same 180 kids, and the switch to Mass Academy was pretty significant.
 		Luckily, it appears to be the right decision so far. I'm not falling asleep in class anymore, and we do cool stuff like build websites.</p>
-		<!--<p>
-		My name ain't Da-Jin<br>
-		You're just imagining<br>
-		It's dj chu<br>
-		And I'd love to meet you<br>
-		</p>-->
+	</div>
+	<div class = "carddiv">
+		<h3>Bedford High School</h3>
+		<h4>My sending school</h4>
+		<p>BHS is a small public high school with around 200 people per grade. 
+		Students mostly come from the town of Bedford, but some also come from Hanscom Air Force Base or from Boston through the METCO system.
+		Bedford is well-known for its social studies program, and has done very well on history fair in the past.</p>
+		<a href="http://bedford.k12.ma.us/home/bedford-high-school.html">BHS website</a>
+		<br>
+		<img src="/images/bhsbuilding.jpg"/>
 	</div>
 	<div class = "carddiv">
 		<h3>Log Frog</h3>
 		<h4>A game I published on Google Play</h4>
-		<p>Log Frog began at CodeDay Spring, a 24-hour coding marathon for students.
+		<p>Log Frog began spring of 2015 at <a href="https://codeday.org/boston">CodeDay</a>, a 24-hour coding marathon for students.
 		I continued to work on it over the summer before junior year, and published it that August.</p>
 		<pre>Log frog is a simple abstract puzzle game.
 Connect, rotate, and leap-frog logs to get to the green circle.
 Moving a log next to another log allows you to move any connected log.
 Includes 40 unique levels that increase in challenge; many will leave you stumped!</pre>
-		<img alt="Log Frog"
-       			src="/images/logfrogicon.png"
-       			width = "100px"/>
        	<a href="https://play.google.com/store/apps/details?id=com.gmail.dajinchu.logfrog">
   			<img alt="Get it on Google Play"
        			src="https://developer.android.com/images/brand/en_generic_rgb_wo_45.png" />
 		</a>
+	</div>
+	<div class = "carddiv">
+		<h3>Destination Imagination</h3>
+		<h4>My life, grades 3-9</h4>
+		<p><a href = "http://www.destinationimagination.org/">Destination Imagination</a> taught me, to name a few, teamwork, STEM principles, creativity, project management, and critical thinking by solving and presenting solutions to an open-ended challenge. 
+		Solutions included presenting a story and building a small structure to hold weight. 
+		My team won the regional and state level tournaments and represented Massachusetts at Global Finals seven times, placing top ten six times, including first and third place. 
+		It has changed me tremendously, and I would not be who I am today, had I not signed up in third grade.</p>
+		<img src = "/images/dilogo.png" />
+		<img src = "/images/structure.jpg" height = "300px"/>
 	</div>
 	</div>
 	</div>
